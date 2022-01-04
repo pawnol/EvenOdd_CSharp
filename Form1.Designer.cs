400639e75969276420de2e98f5361d8ade0e43c5
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-01-03 22:31:44 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EvenOdd
+{
+    partial class EvenOddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkButton = new System.Windows.Forms.Button();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.evenOddLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.Location = new System.Drawing.Point(55, 58);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(239, 45);
+            this.checkButton.TabIndex = 0;
+            this.checkButton.Text = "Check if Even or Odd";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLabel.Location = new System.Drawing.Point(107, 20);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(84, 24);
+            this.numberLabel.TabIndex = 1;
+            this.numberLabel.Text = "Number:";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberTextBox.Location = new System.Drawing.Point(197, 17);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(44, 29);
+            this.numberTextBox.TabIndex = 2;
+            // 
+            // evenOddLabel
+            // 
+            this.evenOddLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.evenOddLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evenOddLabel.Location = new System.Drawing.Point(55, 115);
+            this.evenOddLabel.Name = "evenOddLabel";
+            this.evenOddLabel.Size = new System.Drawing.Size(239, 29);
+            this.evenOddLabel.TabIndex = 3;
+            // 
+            // EvenOddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 161);
+            this.Controls.Add(this.evenOddLabel);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.checkButton);
+            this.Name = "EvenOddForm";
+            this.Text = "Even or Odd";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Label evenOddLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Even or Odd
+ * Pawelski
+ * 1/3/2022
+ * Read the code and run the program a few times so you understand
+ * what the program does and how it works. Once you understand what
+ * the program does and how it works, answer the questions found
+ * on the activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvenOdd
+{
+    public partial class EvenOddForm : Form
+    {
+        public EvenOddForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            int number;
+            number = Convert.ToInt32(numberTextBox.Text);
+            if (number % 2 == 0)
+            {
+                evenOddLabel.Text = number + " is even.";
+            }
+            else
+            {
+                evenOddLabel.Text = number + " is odd.";
+            }
+        }
+    }
+}
